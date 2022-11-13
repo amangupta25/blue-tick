b15589d3cbbccf8abba5388dd58dec8f52c13956
Aman Gupta <aman.g@inmobi.com>
2022-11-13 23:48:23 +0530

    git ignore

--- dist/assets/scripts/turnOffUtil.js
+++
@@ -1,9 +0,0 @@
-var ss = document.getElementById("tweetified-off")
-if(!ss) {
-    var s = document.createElement("script")
-    s.id = "tweetified-off";
-    s.src = chrome.runtime.getURL("/assets/scripts/turnOff.js");
-    s.onload = function () {
-    };
-    document.head.appendChild(s);
-}
--- dist/assets/scripts/turnOnUtil.js
+++
@@ -1,9 +0,0 @@
-var ss = document.getElementById("tweetified")
-if(!ss) {
-    var s = document.createElement("script")
-    s.id = "tweetified";
-    s.src = chrome.runtime.getURL("/assets/scripts/turnOn.js");
-    s.onload = function () {
-    };
-    document.head.appendChild(s);
-}
